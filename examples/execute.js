d813d1f1da50e5c1685cb7600b4167b03812e917
NewtonNiu <qianduanzu@gmail.com>
2014-04-02 21:45:16 +0800

    Create execute.js

---
+++ examples/execute.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Newton on 2014/4/2.
+ */
+var NodeAccess = require('../lib/node-access'),
+    connection = NodeAccess.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-access.mdb');
+    
+NodeAccess.debug = true;
+
+connection
+    .query('SELECT * FROM Users')
+    .on('done', function (data){
+        console.log('Result:'.bold, JSON.stringify(data, null, '  ').bold);
+    })
+    .on('fail', function (data){
+        console.log('Error:'.red.bold, data.bold);
+    });
