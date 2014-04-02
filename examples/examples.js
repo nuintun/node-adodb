f92ee685085d28ace02d4f62143468cc0afc14bc
NewtonNiu <qianduanzu@gmail.com>
2014-04-02 21:39:59 +0800

    modify variable name

--- examples/examples.js
+++ examples/examples.js
@@ -2,10 +2,11 @@
  * Created by Newton on 2014/4/2.
  */
 var NodeAccess = require('../lib/node-access'),
-    example = NodeAccess.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-access.mdb');
+    connection = NodeAccess.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-access.mdb');
     
 NodeAccess.debug = true;
-example
+
+connection
     .query('SELECT * FROM Users')
     .on('done', function (data){
         console.log('Result:'.bold, JSON.stringify(data, null, '  ').bold);
