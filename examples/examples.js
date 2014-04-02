db163e64671f4957cd0e0ed5b576459ad3c648e5
NewtonNiu <qianduanzu@gmail.com>
2014-04-02 21:00:28 +0800

    add error event

--- examples/examples.js
+++ examples/examples.js
@@ -5,6 +5,10 @@ var NodeAccess = require('../lib/node-access'),
     example = NodeAccess.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-access.mdb');
     
 NodeAccess.debug = true;
-example.query('SELECT * FROM Users').on('done', function (data){
-    console.log('Result:'.bold, JSON.stringify(data, null, '  ').bold);
-});
+example.query('SELECT * FROM Users')
+    .on('done', function (data){
+        console.log('Result:'.bold, JSON.stringify(data, null, '  ').bold);
+    })
+    .on('fail', function (data){
+        console.log('Error:'.red.bold, data.bold);
+    });
