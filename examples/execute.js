7f4027bc23bdebbefb35126371da4c3b19df0874
NewtonNiu <qianduanzu@gmail.com>
2014-04-05 17:05:16 +0800

    rename project

--- examples/execute.js
+++ examples/execute.js
@@ -2,7 +2,7 @@
  * Created by Newton on 2014/4/2.
  */
 var ADODB = require('../lib/client'),
-    connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-access.mdb;');
+    connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
     
 ADODB.debug = true;
 
