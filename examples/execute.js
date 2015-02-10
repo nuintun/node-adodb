ef1c8f021f42bd98e14812df3809679b106c41fa
Nuintun <nuintun@qq.com>
2015-02-10 16:48:23 +0800

    adjust code style

--- examples/execute.js
+++ examples/execute.js
@@ -2,12 +2,12 @@
  * Created by Newton on 2014/4/2.
  */
 var ADODB = require('../lib/client'),
-    connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
-    
+  connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
+
 ADODB.debug = true;
 
 connection
-    .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Newton", "Male", 25)')
-    .on('done', function (data){
-        console.log('Result:'.green.bold, JSON.stringify(data, null, '  ').bold);
-    });
+  .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Newton", "Male", 25)')
+  .on('done', function (data){
+    console.log('Result:'.green.bold, JSON.stringify(data, null, '  ').bold);
+  });
--- examples/query.js
+++ examples/query.js
@@ -2,12 +2,12 @@
  * Created by Newton on 2014/4/2.
  */
 var ADODB = require('../lib/client'),
-    connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
-    
+  connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
+
 ADODB.debug = true;
 
 connection
-    .query('SELECT * FROM Users')
-    .on('done', function (data){
-        console.log('Result:'.green.bold, JSON.stringify(data, null, '  ').bold);
-    });
+  .query('SELECT * FROM Users')
+  .on('done', function (data){
+    console.log('Result:'.green.bold, JSON.stringify(data, null, '  ').bold);
+  });
