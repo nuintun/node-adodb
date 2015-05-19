36123286b0c979cc455db70b53f42c6eb3aec623
Nuintun <nuintun@qq.com>
2015-05-19 17:26:51 +0800

    update to version 1.3.1

--- examples/execute.js
+++ examples/execute.js
@@ -1,13 +1,20 @@
 /**
  * Created by Newton on 2014/4/2.
  */
-var ADODB = require('../lib/client'),
-  connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
+
+'use strict';
+
+// External lib
+var ADODB = require('../'),
+  colors = require('colors/safe');
+
+// Variable declaration
+var connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
 
 ADODB.debug = true;
 
 connection
   .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Newton", "Male", 25)')
   .on('done', function (data){
-    console.log('Result:'.green.bold, JSON.stringify(data, null, '  ').bold);
+    console.log(colors.green.bold('Result:'), colors.bold(JSON.stringify(data, null, '  ')));
   });
--- examples/query.js
+++ examples/query.js
@@ -1,13 +1,20 @@
 /**
  * Created by Newton on 2014/4/2.
  */
-var ADODB = require('../lib/client'),
-  connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
+
+'use strict';
+
+// External lib
+var ADODB = require('../'),
+  colors = require('colors/safe');
+
+// Variable declaration
+var connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
 
 ADODB.debug = true;
 
 connection
   .query('SELECT * FROM Users')
   .on('done', function (data){
-    console.log('Result:'.green.bold, JSON.stringify(data, null, '  ').bold);
+    console.log(colors.green.bold('Result:'), colors.bold(JSON.stringify(data, null, '  ')));
   });
