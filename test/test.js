b268fa1f3527337c990c801771ae0ef289c33959
Nuintun <nuintun@qq.com>
2015-06-01 17:17:33 +0800

    add test case

--- test/test.js
+++ test/test.js
@@ -14,94 +14,97 @@ var adodb = path.join(__dirname, 'adodb.js'),
   x64 = fs.existsSync(path.join(sysroot, 'SysWOW64')),
   cscript = path.join(sysroot, x64 ? 'SysWOW64' : 'System32', 'cscript.exe');
 
-console.log(cscript, fs.existsSync(cscript) ? 'exists' : '');
-console.log(adodb, fs.existsSync(adodb) ? 'exists' : '');
+if (fs.existsSync(cscript)) {
+  console.log('Use', cscript);
 
-describe('ADODB', function (){
-  // Variable declaration
-  var connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' + source + ';');
+  describe('ADODB', function (){
+    // Variable declaration
+    var connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' + source + ';');
 
-  it('execute', function (next){
-    connection
-      .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25)')
-      .on('done', function (data){
-        expect(data).to.eql({
-          valid: true,
-          message: 'Execute SQL: INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25) success !'
+    it('execute', function (next){
+      connection
+        .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25)')
+        .on('done', function (data){
+          expect(data).to.eql({
+            valid: true,
+            message: 'Execute SQL: INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25) success !'
+          });
+
+          next();
+        }).on('fail', function (error){
+          expect(error).to.have.key('valid');
+
+          next();
         });
+    });
 
-        next();
-      }).on('fail', function (error){
-        expect(error).to.have.key('valid');
+    it('executeScalar', function (next){
+      connection
+        .executeScalar('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25)', 'SELECT @@Identity AS id')
+        .on('done', function (data){
+          expect(data).to.eql({
+            valid: true,
+            message: 'Execute Scalar SQL: INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25) / SELECT @@Identity AS id success !',
+            records: [{ id: 5 }]
+          });
 
-        next();
-      });
-  });
+          next();
+        }).on('fail', function (error){
+          expect(error).to.have.key('valid');
 
-  it('executeScalar', function (next){
-    connection
-      .executeScalar('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25)', 'SELECT @@Identity AS id')
-      .on('done', function (data){
-        expect(data).to.eql({
-          valid: true,
-          message: 'Execute Scalar SQL: INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25) / SELECT @@Identity AS id success !',
-          records: [{ id: 5 }]
+          next();
         });
+    });
 
-        next();
-      }).on('fail', function (error){
-        expect(error).to.have.key('valid');
+    it('query', function (next){
+      connection
+        .query('SELECT * FROM Users')
+        .on('done', function (data){
+          expect(data).to.eql({
+            valid: true,
+            message: 'Execute SQL: SELECT * FROM Users success !',
+            records: [
+              {
+                UserId: 1,
+                UserName: "Nuintun",
+                UserSex: "Male",
+                UserAge: 25
+              },
+              {
+                UserId: 2,
+                UserName: "Angela",
+                UserSex: "Female",
+                UserAge: 23
+              },
+              {
+                UserId: 3,
+                UserName: "Newton",
+                UserSex: "Male",
+                UserAge: 25
+              },
+              {
+                UserId: 4,
+                UserName: "Nuintun",
+                UserSex: "Male",
+                UserAge: 25
+              },
+              {
+                UserId: 5,
+                UserName: "Alice",
+                UserSex: "Female",
+                UserAge: 25
+              }
+            ]
+          });
 
-        next();
-      });
-  });
+          next();
+        }).on('fail', function (error){
+          expect(error).to.have.key('valid');
 
-  it('query', function (next){
-    connection
-      .query('SELECT * FROM Users')
-      .on('done', function (data){
-        expect(data).to.eql({
-          valid: true,
-          message: 'Execute SQL: SELECT * FROM Users success !',
-          records: [
-            {
-              UserId: 1,
-              UserName: "Nuintun",
-              UserSex: "Male",
-              UserAge: 25
-            },
-            {
-              UserId: 2,
-              UserName: "Angela",
-              UserSex: "Female",
-              UserAge: 23
-            },
-            {
-              UserId: 3,
-              UserName: "Newton",
-              UserSex: "Male",
-              UserAge: 25
-            },
-            {
-              UserId: 4,
-              UserName: "Nuintun",
-              UserSex: "Male",
-              UserAge: 25
-            },
-            {
-              UserId: 5,
-              UserName: "Alice",
-              UserSex: "Female",
-              UserAge: 25
-            }
-          ]
+          next();
         });
-
-        next();
-      }).on('fail', function (error){
-        expect(error).to.have.key('valid');
-
-        next();
-      });
+    });
   });
-});
+} else {
+  console.log('This OS not support node-adodb.');
+}
