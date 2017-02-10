c618e9e571aae34076ff031fa643756f1f70282e
nuintun <nuintun@qq.com>
2017-02-10 12:09:08 +0800

    version 3.0.0

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-/**
- * Created by Newton on 2014/4/4.
- */
-module.exports = require('./lib/client');
+'use strict';
+
+var utils = require('./lib/utils');
+var proxy = require('./lib/proxy');
+
+function ADODB(connection) {
+  if (!(this instanceof ADODB)) {
+    return new ADODB(connection);
+  }
+
+  this.connection = connection;
+}
+
+ADODB.prototype = {
+  execute: function(sql, scalar) {
+    var params = {
+      connection: this.connection,
+      sql: sql
+    };
+
+    var command = 'execute';
+
+    if (utils.isString(scalar)) {
+      command = 'executeScalar';
+      params.scalar = scalar;
+    }
+
+    return proxy.exec(command, params);
+  },
+  query: function(sql) {
+    return proxy.exec('query', {
+      connection: this.connection,
+      sql: sql
+    });
+  }
+}
+
+module.exports = {
+  open: ADODB.bind(ADODB)
+};
