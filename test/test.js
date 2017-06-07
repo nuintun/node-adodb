f2cc717614ac3a1847248f9705f0bed0c4bbf19e
nuintun <nuintun@qq.com>
2017-06-07 16:59:19 +0800

    add test case

--- test/test.js
+++ test/test.js
@@ -18,88 +18,111 @@ if (fs.existsSync(cscript) && fs.existsSync(source)) {
   console.log('Use:', cscript);
   console.log('Database:', source);
 
+  // variable declaration
+  var connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' + source + ';');
+
   describe('ADODB', function() {
-    // variable declaration
-    var connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' + source + ';');
+    it('events bind', function(next) {
+      var time = 5;
+      var query = connection.query('SELECT * FROM Users');
 
-    var query = connection.query('SELECT * FROM Users');
-
-    // noop function
-    function fn() {}
-
-    // coveralls cover
-    query
-      .on('done', fn)
-      .off('done', fn)
-      .off('done')
-      .off()
-      .once('done', fn);
-
-    query.on('custom', fn);
-    query.emit('custom');
-    query.emit('custom', 1);
-    query.emit('custom', 1, 2);
-    query.emit('custom', 1, 2, 3);
-    query.emit('custom', 1, 2, 3, 4);
-
-    it('query', function(next) {
-      connection
-        .query('SELECT * FROM Users')
-        .on('done', function(data, message) {
-          expect(data).to.eql(
-            [
-              {
-                UserId: 1,
-                UserName: "Nuintun",
-                UserSex: "Male",
-                UserAge: 25
-              },
-              {
-                UserId: 2,
-                UserName: "Angela",
-                UserSex: "Female",
-                UserAge: 23
-              },
-              {
-                UserId: 3,
-                UserName: "Newton",
-                UserSex: "Male",
-                UserAge: 25
-              }
-            ]
-          );
-          expect(message).to.eql('SELECT * FROM Users success');
-
+      // noop function
+      function fn() {
+        if (!time--) {
           next();
-        }).on('fail', function(error) {
-          next(error);
-        });
+        }
+      }
+
+      // coveralls cover
+      query
+        .on('done', fn)
+        .off('done', fn)
+        .off('done')
+        .off()
+        .once('done', fn);
+
+      query.on('custom', fn);
+      query.emit('custom');
+      query.emit('custom', 1);
+      query.emit('custom', 1, 2);
+      query.emit('custom', 1, 2, 3);
+      query.emit('custom', 1, 2, 3, 4);
     });
 
-    it('execute', function(next) {
-      connection
-        .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25)')
-        .on('done', function(data, message) {
-          expect(data).to.eql([]);
-          expect(message).to.eql('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25) success');
+    it('resolve type', function(next) {
+      expect(ADODB.resolveType(202)).to.eql('adVarWChar');
 
-          next();
-        }).on('fail', function(error) {
-          next(error);
-        });
+      next();
     });
 
-    it('scalar', function(next) {
-      connection
-        .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25)', 'SELECT @@Identity AS id')
-        .on('done', function(data, message) {
-          expect(data).to.eql([{ id: 5 }]);
-          expect(message).to.eql('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25) / SELECT @@Identity AS id success');
+    it('resolve attribute', function(next) {
+      expect(ADODB.resolveAttr(106)).to.eql({
+        adFldMayDefer: true,
+        adFldUnknownUpdatable: true,
+        adFldIsNullable: true,
+        adFldMayBeNull: true
+      });
 
-          next();
-        }).on('fail', function(error) {
-          next(error);
-        });
+      next();
+    });
+
+    describe('query', function() {
+      it('no field description', function(next) {
+        connection
+          .query('SELECT * FROM Users')
+          .on('done', function(data, message) {
+            expect(data.length).to.eql(3);
+            expect(data[0].UserName).to.eql('Nuintun');
+            expect(message).to.eql('SELECT * FROM Users success');
+
+            next();
+          }).on('fail', function(error) {
+            next(error);
+          });
+      });
+
+      it('with field description', function(next) {
+        connection
+          .query('SELECT * FROM Users', true)
+          .on('done', function(data, message) {
+            expect(data.length).to.eql(3);
+            expect(data[0].UserName.Type).to.eql(202);
+            expect(data[0].UserName.Value).to.eql('Nuintun');
+            expect(message).to.eql('SELECT * FROM Users success');
+
+            next();
+          }).on('fail', function(error) {
+            next(error);
+          });
+      });
+    });
+
+    describe('execute', function() {
+      it('no scalar', function(next) {
+        connection
+          .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25)')
+          .on('done', function(data, message) {
+            expect(data).to.eql([]);
+            expect(message).to.eql('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Nuintun", "Male", 25) success');
+
+            next();
+          }).on('fail', function(error) {
+            next(error);
+          });
+      });
+
+      it('with scalar', function(next) {
+        connection
+          .execute('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25)', 'SELECT @@Identity AS id')
+          .on('done', function(data, message) {
+            expect(data).to.eql([{ id: 5 }]);
+            expect(message).to.eql('INSERT INTO Users(UserName, UserSex, UserAge) VALUES ("Alice", "Female", 25) / SELECT @@Identity AS id success');
+
+            next();
+          }).on('fail', function(error) {
+            next(error);
+          });
+      });
     });
   });
 } else {
