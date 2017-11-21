2ae70e66cd6aa879e292e05438e6c195bec32554
nuintun <nuintun@qq.com>
2017-11-21 17:58:15 +0800

    :sparkles: support binary

--- query.js
+++ query.js
@@ -3,7 +3,6 @@
 // External lib
 var ADODB = require('./');
 var fs = require('fs');
-var jpeg = require('jpeg-js');
 
 // Variable declaration
 const connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=node-adodb.mdb;');
@@ -14,12 +13,7 @@ process.env.DEBUG = 'ADODB';
 connection
   .query('SELECT * FROM Users')
   .then(data => {
-    // console.log(JSON.stringify(data, null, 2));
-    // var jpg = jpeg.decode(Buffer.from(data[0].UserAvatar, 'binary'));
-
     console.log(data[0].UserAvatar);
-
-    fs.writeFileSync('./avatar.jpg', Buffer.from(data[0].UserAvatar));
   })
   .catch(error => {
     console.log(error);
